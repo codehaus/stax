761571095b4797951f6b89565f19d8a005d0eedf
cowtowncoder <cowtowncoder@5398dcc7-d8e7-0310-b5b9-f85cff58d174>
2005-03-05 00:10:53 +0000

    Added new unit tests for stream writer, now checks for some of stream writer functionality.

--- trunk/dev/src/org/codehaus/stax/test/wstream/TestRepairingWriter.java
+++ trunk/dev/src/org/codehaus/stax/test/wstream/TestRepairingWriter.java
@@ -11,11 +11,108 @@ import javax.xml.stream.*;
 public class TestRepairingWriter
     extends BaseWriterTest
 {
-    public void testDummy()
+    /**
+     * Test similar to the one in {@link TestSimpleWriter}.
+     */
+    public void testElements()
         throws IOException, XMLStreamException
     {
         StringWriter strw = new StringWriter();
         XMLStreamWriter w = getRepairingWriter(strw);
-        // !!! TBI
+        final String URL_P1 = "http://p1.org";
+        final String URL_P2 = "http://ns.p2.net/yeehaw.html";
+        final String URL_DEF = "urn:default";
+
+        final String TEXT = "  some text\n";
+
+        w.writeStartDocument();
+
+        /* Calling setPrefix() should be optional; but if we call it,
+         * exceptation is that it does properly cause URL to be bound.
+         */
+        w.setPrefix("p1", URL_P1);
+        w.writeStartElement(URL_P1, "test");
+
+        w.writeStartElement("p2", "branch", URL_P2);
+
+        // And then a dynamically created prefix...
+        w.writeStartElement(URL_DEF, "leaf");
+
+        w.writeCharacters(TEXT);
+
+        w.writeEndElement(); // first leaf
+
+        w.writeEmptyElement(URL_P1, "leaf"); // second leaf
+
+        w.writeStartElement("", "third"); // may need dynamic NS too
+        w.writeEndElement();
+
+        w.writeEndElement(); // branch
+        w.writeEndElement(); // root elem
+        w.writeEndDocument();
+        w.close();
+
+        /* And then let's parse and verify it all:
+         */
+
+        XMLStreamReader sr = constructNsStreamReader(strw.toString());
+        assertTokenType(START_DOCUMENT, sr.getEventType());
+
+        // root element
+        assertTokenType(START_ELEMENT, sr.next());
+        assertEquals("test", sr.getLocalName());
+        // ??? is writer obligated to honor the prefix suggestion
+        assertEquals(URL_P1, sr.getNamespaceURI());
+        /* note: can not really verify number of namespace bindings, since
+         * writer should be in charge... and it may output extra bindings,
+         * too (and use default ns or explicit ones etc)
+         */
+        
+        // first branch:
+        assertTokenType(START_ELEMENT, sr.next());
+        assertEquals("branch", sr.getLocalName());
+        assertEquals(URL_P2, sr.getNamespaceURI());
+
+        // first leaf
+        assertTokenType(START_ELEMENT, sr.next());
+        assertEquals("leaf", sr.getLocalName());
+        assertEquals(URL_DEF, sr.getNamespaceURI());
+
+        assertTokenType(CHARACTERS, sr.next());
+        assertEquals(TEXT, getAllText(sr));
+        // not: getAllText ^^^ moves cursor!
+
+        assertTokenType(END_ELEMENT, sr.getEventType());
+        assertEquals("leaf", sr.getLocalName());
+        assertEquals(URL_DEF, sr.getNamespaceURI());
+
+        // another leaf:
+        assertTokenType(START_ELEMENT, sr.next());
+        assertEquals("leaf", sr.getLocalName());
+        assertEquals(URL_P1, sr.getNamespaceURI());
+
+        assertTokenType(END_ELEMENT, sr.next());
+        assertEquals("leaf", sr.getLocalName());
+        assertEquals(URL_P1, sr.getNamespaceURI());
+
+        // "third"
+        assertTokenType(START_ELEMENT, sr.next());
+        assertEquals("third", sr.getLocalName());
+        assertNoNsURI(sr);
+        assertTokenType(END_ELEMENT, sr.next());
+        assertEquals("third", sr.getLocalName());
+        assertNoNsURI(sr);
+
+        // (close) branch
+        assertTokenType(END_ELEMENT, sr.next());
+        assertEquals("branch", sr.getLocalName());
+        assertEquals(URL_P2, sr.getNamespaceURI());
+
+        // closing root element
+        assertTokenType(END_ELEMENT, sr.next());
+        assertEquals("test", sr.getLocalName());
+        assertEquals(URL_P1, sr.getNamespaceURI());
+
+        assertTokenType(END_DOCUMENT, sr.next());
     }
 }
