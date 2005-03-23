e0a466c54ee566763baf24e6481bf6dc50fc783e
cowtowncoder <cowtowncoder@5398dcc7-d8e7-0310-b5b9-f85cff58d174>
2005-03-23 01:14:25 +0000

    Added more unit tests for checking that xml document encoding (auto-) detection works ok with and without BOMs, and explicit declarations. Also, fixed a small problem with building test code; reference to junit jar wasn't correct.

--- dev/src/org/codehaus/stax/test/stream/TestEncodingRead.java
+++ dev/src/org/codehaus/stax/test/stream/TestEncodingRead.java
@@ -30,17 +30,108 @@ public class TestEncodingRead
         +UTF_3 + UTF_2 + UTF_1
         ;
 
+    final static byte[] BE_BOM = new byte[] { (byte) 0xFE, (byte) 0xFF };
+    final static byte[] LE_BOM = new byte[] { (byte) 0xFF, (byte) 0xFE };
+    final static byte[] UTF8_BOM = new byte[] { (byte) 0xEF, (byte) 0xBB, (byte) 0xBF };
+
     public void testUTF8()
         throws XMLStreamException,
                UnsupportedEncodingException
     {
-        String XML = "<?xml version='1.0' encoding=\"UTF-8\"  ?>"
-            +"<root>"+UTF_CONTENT+"</root>";
-        byte[] b = XML.getBytes("UTF-8");
+	doTestEncoding("UTF-8", true, null);
+	doTestEncoding("UTF-8", false, null);
+	doTestEncoding("UTF-8", true, UTF8_BOM);
+	doTestEncoding("UTF-8", false, UTF8_BOM);
+    }
+
+    public void testUTF16()
+        throws XMLStreamException,
+               UnsupportedEncodingException
+    {
+	/* Not 100% sure on how to test it... I know JDK defaults to
+	 * big-endian... could maybe write using BE/LE variants, but
+	 * include declaration of plain 'UTF-16' instead?
+	 */
+	/* For now, let's trust JDK to add the BE BOM, and leave it
+	 * at that.
+	 */
+	doTestEncoding("UTF-16", true, null);
+	doTestEncoding("UTF-16", false, null);
+    }
+
+    public void testUTF16BEWithBOM()
+        throws XMLStreamException,
+               UnsupportedEncodingException
+    {
+	doTestEncoding("UTF-16BE", true, BE_BOM);
+	doTestEncoding("UTF-16BE", false, BE_BOM);
+    }
+
+    public void testUTF16BEWithoutBOM()
+        throws XMLStreamException,
+               UnsupportedEncodingException
+    {
+	doTestEncoding("UTF-16BE", true, null);
+	doTestEncoding("UTF-16BE", false, null);
+    }
+
+    public void testUTF16LEWithBOM()
+        throws XMLStreamException,
+               UnsupportedEncodingException
+    {
+	doTestEncoding("UTF-16LE", true, LE_BOM);
+	doTestEncoding("UTF-16LE", false, LE_BOM);
+    }
+
+    public void testUTF16LEWithoutBOM()
+        throws XMLStreamException,
+               UnsupportedEncodingException
+    {
+	doTestEncoding("UTF-16LE", true, null);
+	doTestEncoding("UTF-16LE", false, null);
+    }
+
+    /*
+    ////////////////////////////////////////
+    // Private methods, shared test code
+    ////////////////////////////////////////
+     */
+
+    public void doTestEncoding(String enc, boolean explicit,
+			       byte[] bom)
+        throws XMLStreamException,
+               UnsupportedEncodingException
+    {
+        String XML = "<?xml version='1.0'";
+
+	if (explicit) {
+	    XML +=  " encoding='"+enc+"'";
+	}
+	XML += "?><root>"+UTF_CONTENT+"</root>";
+
+        byte[] b = XML.getBytes(enc);
+	if (bom != null) {
+	    /* 22-Mar-2005, TSa: Hack! I'll be damned if I know for sure
+	     *   whether JDK is going to add BOMs... so let's just add
+	     *   a special check to make sure no duplicate BOMs are added.
+	     */
+	    if (bom[0] == b[0]) {
+		System.err.println("Warning: JDK implicitly added BOM for '"+enc+"'; won't try to add it implicitly...");
+	    } else {
+		byte[] orig = b;
+		b = new byte[b.length + bom.length];
+		System.arraycopy(bom, 0, b, 0, bom.length);
+		System.arraycopy(orig, 0, b, bom.length, orig.length);
+	    }
+	}
 
         XMLStreamReader sr = getReader(b);
 
-        assertEquals("UTF-8", sr.getCharacterEncodingScheme());
+	if (explicit) {
+	    assertEquals(enc, sr.getCharacterEncodingScheme());
+	} else {
+	    // otherwise... should we get some info?
+	}
 
         assertEquals(START_ELEMENT, sr.next());
         assertEquals(CHARACTERS, sr.next());
