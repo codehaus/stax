c95703ec038f4883038fa8e7d559c11e32122d06
cowtowncoder <cowtowncoder@5398dcc7-d8e7-0310-b5b9-f85cff58d174>
2005-12-28 06:11:00 +0000

    One more unit test to check attribute definition precedence when combining internal and external subsets.

--- trunk/dev/src/org/codehaus/stax/test/vstream/TestSubsetCombination.java
+++ trunk/dev/src/org/codehaus/stax/test/vstream/TestSubsetCombination.java
@@ -27,16 +27,46 @@ public class TestSubsetCombination
             +"<!ELEMENT root (leaf+)>\n"
             +"<!ATTLIST root attrInt CDATA #IMPLIED>\n"
             +"<!ENTITY ent1 '&ent2;'>\n"
-            +"]><root attrInt='value' attrExt='someValue'>  <leaf>Test entities: &ent1, &ent2;</leaf>"
-            +"]><root attrInt='value'>  <leaf>Test entities: &ent1, &ent2;</leaf>"
+            +"]><root attrInt='value' attrExt='someValue'>  <leaf>Test entities: &ent1;, &ent2;</leaf>"
+            +"<leaf>...</leaf>"
             +"<leaf /></root>";
         String EXT_DTD = 
             "<!ELEMENT leaf (#PCDATA)>\n"
+            +"<!ENTITY ent2 'some text'>\n"
             +"<!ATTLIST root attrExt CDATA #IMPLIED>\n"
             ;
         streamThrough(getReader(XML, true, EXT_DTD));
         // Let's also test that non-ns works, just in case it's different
         streamThrough(getReader(XML, false, EXT_DTD));
+    }
+
+    /**
+     * This unit test checks that the internal subset has precedence
+     * for attribute definitions -- it's ok to declare attributes multiple
+     * times, but the first one sticks, and internal subset is considered
+     * to come before external subset
+     */
+    public void testAttributePrecedence()
+        throws XMLStreamException
+    {
+        String XML =
+            "<!DOCTYPE root SYSTEM 'dummy-url' [\n"
+            +"<!ELEMENT root EMPTY>\n"
+            +"<!ATTLIST root attr1 CDATA #IMPLIED>\n"
+            +"<!ATTLIST root attr2 CDATA 'intValue'>\n"
+            +"]><root />"
+            ;
+        String EXT_DTD = 
+            "<!ATTLIST root attr1 CDATA 'extValue'>\n"
+            +"<!ATTLIST root attr2 CDATA #IMPLIED>\n"
+            ;
+        XMLStreamReader sr = getReader(XML, true, EXT_DTD);
+        assertTokenType(DTD, sr.next());
+        assertTokenType(START_ELEMENT, sr.next());
+        assertEquals(1, sr.getAttributeCount());
+        assertEquals("attr2", sr.getAttributeLocalName(0));
+        assertEquals("intValue", sr.getAttributeValue(0));
+        assertFalse(sr.isAttributeSpecified(0));
     }
 
     /*
@@ -57,6 +87,8 @@ public class TestSubsetCombination
         setValidating(f, true);
         if (extSubset != null) {
             setResolver(f, new SimpleResolver(extSubset));
+        } else {
+            setResolver(f, null);
         }
         return constructStreamReader(f, contents);
     }
