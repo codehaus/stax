c4bffa1f7238cac59dfdaddbe07c9219f1833e1e
cowtowncoder <cowtowncoder@5398dcc7-d8e7-0310-b5b9-f85cff58d174>
2006-04-09 05:14:18 +0000

    Fixed StaxTest encoding unit tests, to remove unnecessary testing of Readers (who shouldn't worry about BOMs) and fix some issues regarding Java vs. xml encoding declarations (byte-order in encoding names for Java, not for xml).

--- trunk/dev/src/org/codehaus/stax/test/stream/TestEncodingRead.java
+++ trunk/dev/src/org/codehaus/stax/test/stream/TestEncodingRead.java
@@ -34,47 +34,26 @@ public class TestEncodingRead
     final static byte[] LE_BOM = new byte[] { (byte) 0xFF, (byte) 0xFE };
     final static byte[] UTF8_BOM = new byte[] { (byte) 0xEF, (byte) 0xBB, (byte) 0xBF };
 
+    /**
+     * Test to check that UTF-8 stream with no leading BOM is succesfully
+     * handled by parser.
+     */
     public void testUTF8()
         throws Exception
     {
-        doTestEncoding("UTF-8", true, null);
-        doTestEncoding("UTF-8", false, null);
-    }
-
-    public void testUTF8WithBOM()
-        throws Exception
-    {
-        doTestEncoding("UTF-8", true, UTF8_BOM);
-        doTestEncoding("UTF-8", false, UTF8_BOM);
+        doTestEncoding("UTF-8", "UTF-8", null);
+        doTestEncoding("UTF-8", null, null);
     }
 
     /**
-     * This unit test checks specifically whether the implementation can
-     * use a BOM-tagged Reader; JDK seems to (sometimes?) leave BOM in
-     * the Reader's stream...
+     * Test to check that UTF-8 stream with leading BOM is succesfully
+     * handled by parser.
      */
-    public void testUTF8ViaReader()
-        throws XMLStreamException, IOException
+    public void testUTF8WithBOM()
+        throws Exception
     {
-        String XML = "...<?xml version='1.0' encoding='UTF-8'?><root></root>";
-        byte[] b = XML.getBytes("UTF-8");
-        b[0] = (byte) 0xEF;
-        b[1] = (byte) 0xBB;
-        b[2] = (byte) 0xBF;
-        
-        InputStreamReader reader = new InputStreamReader(new ByteArrayInputStream(b), "UTF-8");
-        
-        try {
-            XMLInputFactory f = getInputFactory();
-            setValidating(f, false);
-            XMLStreamReader sr = f.createXMLStreamReader(reader);
-            assertTokenType(START_DOCUMENT, sr.getEventType());
-            assertTokenType(START_ELEMENT, sr.next());
-            assertEquals("root", sr.getLocalName());
-            sr.close();
-        } finally {
-            reader.close();
-        }
+        doTestEncoding("UTF-8", "UTF-8", UTF8_BOM);
+        doTestEncoding("UTF-8", null, UTF8_BOM);
     }
 
     public void testUTF8Surrogates()
@@ -105,55 +84,24 @@ public class TestEncodingRead
         assertTokenType(END_ELEMENT, sr.next());
     }
 
-    public void testUTF16()
-        throws XMLStreamException,
-               UnsupportedEncodingException
-    {
-        /* Not 100% sure on how to test it... I know JDK defaults to
-         * big-endian... could maybe write using BE/LE variants, but
-         * include declaration of plain 'UTF-16' instead?
-         */
-        /* For now, let's trust JDK to add the BE BOM, and leave it
-         * at that.
-         */
-        doTestEncoding("UTF-16", true, null);
-        doTestEncoding("UTF-16", false, null);
-    }
-
     public void testUTF16BEWithBOM()
         throws XMLStreamException,
                UnsupportedEncodingException
     {
-        doTestEncoding("UTF-16BE", true, BE_BOM);
-        doTestEncoding("UTF-16BE", false, BE_BOM);
+        doTestEncoding("UTF-16BE", "UTF-16", BE_BOM);
+        doTestEncoding("UTF-16BE", null, BE_BOM);
 
         doTestEncoding2(true);
-    }
-
-    public void testUTF16BEWithoutBOM()
-        throws XMLStreamException,
-               UnsupportedEncodingException
-    {
-        doTestEncoding("UTF-16BE", true, null);
-        doTestEncoding("UTF-16BE", false, null);
     }
 
     public void testUTF16LEWithBOM()
         throws XMLStreamException,
                UnsupportedEncodingException
     {
-        doTestEncoding("UTF-16LE", true, LE_BOM);
-        doTestEncoding("UTF-16LE", false, LE_BOM);
+        doTestEncoding("UTF-16LE", "UTF-16", LE_BOM);
+        doTestEncoding("UTF-16LE", null, LE_BOM);
 
         doTestEncoding2(false);
-    }
-
-    public void testUTF16LEWithoutBOM()
-        throws XMLStreamException,
-               UnsupportedEncodingException
-    {
-        doTestEncoding("UTF-16LE", true, null);
-        doTestEncoding("UTF-16LE", false, null);
     }
 
     /*
@@ -162,26 +110,33 @@ public class TestEncodingRead
     ////////////////////////////////////////
      */
 
-    public void doTestEncoding(String enc, boolean explicit,
-			       byte[] bom)
+    /**
+     * @param javaEnc Name of encoding as understood by JDK; used to
+     *    instantiate JDK encoder/decoder to use for test
+     * @param xmlEnc Name of encoding as included in xml declaration;
+     *   null to indicate nothing should be added
+     * @param bom Pre-defined bom bytes to prepend to input, if any.
+     */
+    public void doTestEncoding(String javaEnc, String xmlEnc,
+                               byte[] bom)
         throws XMLStreamException,
                UnsupportedEncodingException
     {
         String XML = "<?xml version='1.0'";
         
-        if (explicit) {
-            XML +=  " encoding='"+enc+"'";
+        if (xmlEnc != null) {
+            XML +=  " encoding='"+xmlEnc+"'";
         }
         XML += "?><root>"+UTF_CONTENT+"</root>";
         
-        byte[] b = XML.getBytes(enc);
+        byte[] b = XML.getBytes(javaEnc);
         if (bom != null) {
             /* 22-Mar-2005, TSa: Hack! I'll be damned if I know for sure
              *   whether JDK is going to add BOMs... so let's just add
              *   a special check to make sure no duplicate BOMs are added.
              */
             if (bom[0] == b[0]) {
-                System.err.println("Warning: JDK implicitly added BOM for '"+enc+"'; won't try to add it implicitly...");
+                System.err.println("Warning: JDK implicitly added BOM for '"+xmlEnc+"'/'"+javaEnc+"'; won't try to add it implicitly...");
             } else {
                 byte[] orig = b;
                 b = new byte[b.length + bom.length];
@@ -192,18 +147,21 @@ public class TestEncodingRead
         
         XMLStreamReader sr = getReader(b);
         
-        if (explicit) {
-            assertEquals(enc, sr.getCharacterEncodingScheme());
+        if (xmlEnc != null) {
+            assertEquals(xmlEnc, sr.getCharacterEncodingScheme());
         } else {
-            // otherwise... should we get some info?
+            /* otherwise... should we get some info? Preferably yes;
+             * (getEncoding() should return auto-detected encoding)
+             * but this is not strictly mandated by the specs?
+             */
         }
         
         assertEquals(START_ELEMENT, sr.next());
         assertEquals(CHARACTERS, sr.next());
         
-        assertEquals(UTF_CONTENT, getAndVerifyText(sr));
+        assertEquals(UTF_CONTENT, getAllText(sr));
 
-        assertEquals(END_ELEMENT, sr.next());
+        assertEquals(END_ELEMENT, sr.getEventType());
         assertEquals(END_DOCUMENT, sr.next());
     }
 
