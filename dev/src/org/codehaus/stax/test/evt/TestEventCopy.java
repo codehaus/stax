2a85463460ead1db3b5318eb3f37c6f5f575b432
cowtowncoder <cowtowncoder@5398dcc7-d8e7-0310-b5b9-f85cff58d174>
2007-12-05 06:45:47 +0000

    Adding more comprehensive unit tests for copying (writing) event objects

--- dev/src/org/codehaus/stax/test/evt/TestEventCopy.java
+++ dev/src/org/codehaus/stax/test/evt/TestEventCopy.java
@@ -2,6 +2,7 @@ package org.codehaus.stax.test.evt;
 
 import java.util.NoSuchElementException;
 
+import javax.xml.namespace.QName;
 import javax.xml.stream.*;
 import javax.xml.stream.events.*;
 
@@ -28,7 +29,7 @@ public class TestEventCopy
             +" <leaf attr='\"a\"' />"
             +"</root>"
             ;
-		XMLEventReader er = getEventReader(XML, true);
+		XMLEventReader er = getEventReader(XML, true, true);
         StringWriter strw = new StringWriter();
         XMLOutputFactory f = getOutputFactory();
         XMLEventWriter ew = f.createXMLEventWriter(strw);
@@ -39,20 +40,106 @@ public class TestEventCopy
         ew.close();
 
         // And let's then just verify it's well-formed still
-        // (should also check it's still the same, too...)
         String results = strw.toString();
-        er = getEventReader(results, true);
-		while (er.hasNext()) {
-            assertNotNull(er.nextEvent());
-        }
+        er = getEventReader(results, true, true);
+        XMLEvent evt;
+
+        // Plus that events are the way they ought to be
+        assertNotNull((evt = er.nextEvent()));
+        assertTrue(evt.isStartDocument());
+
+        evt = er.nextEvent();
+        assertEquals(new QName("root"), evt.asStartElement().getName());
+
+        evt = er.nextEvent();
+        assertTrue(evt.isCharacters());
+        assertTrue(evt.asCharacters().isWhiteSpace());
+
+        evt = er.nextEvent();
+        assertTrue(evt.isStartElement());
+        assertEquals(new QName("branch"), evt.asStartElement().getName());
+
+        evt = er.nextEvent();
+        assertTrue(evt.isCharacters());
+        assertTrue(evt.asCharacters().isWhiteSpace());
+
+        evt = er.nextEvent();
+        assertTrue(evt.isStartElement());
+        assertEquals(new QName("leaf"), evt.asStartElement().getName());
+        evt = er.nextEvent();
+        assertTrue(evt.isEndElement());
+        assertEquals(new QName("leaf"), evt.asEndElement().getName());
+
+        evt = er.nextEvent();
+        assertTrue(evt.isCharacters());
+        assertTrue(evt.asCharacters().isWhiteSpace());
+
+        evt = er.nextEvent();
+        assertTrue(evt.isEndElement());
+        assertEquals(new QName("branch"), evt.asEndElement().getName());
+
+        evt = er.nextEvent();
+        assertTrue(evt.isCharacters());
+        assertTrue(evt.asCharacters().isWhiteSpace());
+
+        evt = er.nextEvent();
+        assertTrue(evt.isStartElement());
+        assertEquals(new QName("leaf"), evt.asStartElement().getName());
+        evt = er.nextEvent();
+        assertTrue(evt.isEndElement());
+        assertEquals(new QName("leaf"), evt.asEndElement().getName());
+
+        evt = er.nextEvent();
+        assertTrue(evt.isEndElement());
+        assertEquals(new QName("root"), evt.asEndElement().getName());
 	}
 
-    private XMLEventReader getEventReader(String contents, boolean nsAware)
+	public void testCopyWithCData()
+        throws XMLStreamException
+    {
+        final String XML =
+            "<root><![CDATA[a&b]]></root>";
+
+		XMLEventReader er = getEventReader(XML, true, false);
+        StringWriter strw = new StringWriter();
+        XMLOutputFactory f = getOutputFactory();
+        XMLEventWriter ew = f.createXMLEventWriter(strw);
+
+		while (er.hasNext()) {
+            ew.add(er.nextEvent());
+		}
+        ew.close();
+
+        // And then test what it looks like
+        String results = strw.toString();
+        er = getEventReader(results, true, false);
+        XMLEvent evt;
+
+        // Plus that events are the way they ought to be
+        assertNotNull((evt = er.nextEvent()));
+        assertTrue(evt.isStartDocument());
+
+        evt = er.nextEvent();
+        assertTrue(evt.isStartElement());
+        assertEquals(new QName("root"), evt.asStartElement().getName());
+
+        evt = er.nextEvent();
+        assertTrue(evt.isCharacters());
+        assertTrue(evt.asCharacters().isCData());
+        assertEquals("a&b", evt.asCharacters().getData());
+
+        evt = er.nextEvent();
+        assertTrue(evt.isEndElement());
+        assertEquals(new QName("root"), evt.asEndElement().getName());
+
+    }
+
+    private XMLEventReader getEventReader(String contents, boolean nsAware, boolean coal)
         throws XMLStreamException
     {
         XMLInputFactory f = getInputFactory();
         setNamespaceAware(f, nsAware);
-        setCoalescing(f, true);
+        setCoalescing(f, coal);
         setSupportDTD(f, true);
         setValidating(f, false);
         return constructEventReader(f, contents);
